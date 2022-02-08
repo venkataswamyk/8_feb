cf5441047839afe52406fe327286486bdda092db
venkataswamyk <99276480+venkataswamyk@users.noreply.github.com>
2022-02-08 22:16:43 +0530

    done Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+a=input()
+b=input()
+
+if a>b:
+    print(a," is greather than ",b)
+    
+if a<b:
+    print(a,"is less than",b)
+    
+if a==b:
+    print(a,"is equal to",b)
+    
+if a>=b:
+    print(a,"is greater than or equal",b)
+    
+if a<=b:
+    print(a,"is less than equal",b)
+    
+if a!=b:
+    print(a,"is not equal",b)
