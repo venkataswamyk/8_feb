230ff45079a35e68b8cfa884244d55c2c06ddc1d
venkataswamyk <99276480+venkataswamyk@users.noreply.github.com>
2022-02-08 22:21:54 +0530

    done Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,7 @@
+# This is a Python program which converts the value of Kilometer into cm   
+      
+Kilometer=int(input("Enter the length in Kilometer:"))  
+#convert Kilometer to cm  
+centimeter  = 100000 * Kilometer;    
+     
+print("The length in centimeter",round(centimeter,2))
